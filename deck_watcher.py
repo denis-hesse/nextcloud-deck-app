a78e9d0a7932e4f4801424f4c6cd710ae84f4bd5
denis-hesse <contact@dslconseil.fr>
2026-05-04 10:45:19 +0200

    Add files via upload

--- deck_watcher.py
+++ deck_watcher.py
@@ -208,7 +208,7 @@ def fetch_deck_mails(cfg, processed):
             return []
 
         # Chercher mails depuis hier
-        since = (datetime.now() - timedelta(days=1)).strftime("%d-%b-%Y")
+        since = (datetime.now() - timedelta(hours=1)).strftime("%d-%b-%Y")
         _, data = mail.search(None, f'SINCE {since}')
         mail_ids = data[0].split()
         print(f"  {len(mail_ids)} mail(s) trouvés depuis hier", flush=True)
